07303f098ed0c0f49a786ac808dc3065f0c0d0e9
mgimon <m.gimoncaballero@gmail.com>
2026-02-11 13:08:41 +0100

    Validacion de rutas por sesiones de usuarios

--- srcs/containers/user/user-service/src/handlers/users.js
+++ srcs/containers/user/user-service/src/handlers/users.js
@@ -4,7 +4,6 @@ import util from 'node:util'
 import { pipeline } from 'node:stream'
 import { unlink, access, constants } from 'fs/promises'
 import path from 'node:path'
-import jwt from 'jsonwebtoken';
 
 const pump = util.promisify(pipeline)
 
@@ -218,80 +217,6 @@ async function deleteAvatar(req, reply) {
         reply.send(error)
     }
 }
-
-
-async function verifySession(request, reply) {
-    const token = request.cookies?.access_token;
-
-    if (!token) {
-        console.log('[verifySession] No se recibió token');
-        return reply.code(401).send({ message: 'No hay una sesión iniciada' });
-    }
-
-    try {
-        request.user = jwt.verify(token, process.env.JWT_SECRET);
-        request.headers['x-user-id'] = request.user.userId;
-        request.headers['x-user-role'] = request.user.role;
-    } catch (err) {
-        console.log('[verifySession] JWT error:', err.message);
-        return reply.code(401).send({ message: 'No hay una sesión iniciada' });
-    }
-}
-
-async function verifySessionFromPath(request, reply) {
-    const token = request.cookies?.access_token;
-
-    if (!token) {
-        console.log('[verifySessionFromPath] No se recibió token');
-        return reply.code(401).send({ message: 'No hay una sesión iniciada' });
-    }
-
-    try {
-        request.user = jwt.verify(token, process.env.JWT_SECRET);
-        request.headers['x-user-id'] = request.user.userId;
-        request.headers['x-user-role'] = request.user.role;
-
-        const { userId } = request.params; // de la url /:userId/etc
-
-        if (Number(userId) !== Number(request.user.userId))
-        {
-            return reply.code(403).send({
-                message: 'No autorizado para acceder a este recurso',
-                debug: { routeUserId: userId, sessionUserId: request.user.userId }
-            });
-        }
-
-    } catch (err) {
-        console.log('[verifySessionFromPath] JWT error:', err.message);
-        return reply.code(401).send({ message: 'No hay una sesión iniciada' });
-    }
-}
-
-
-async function verifySessionFromBody(request, reply) {
-    const token = request.cookies?.access_token;
-
-    if (!token) {
-        console.log('[verifySessionFromBody] No se recibió token');
-        return reply.code(401).send({ message: 'No hay una sesión iniciada' });
-    }
-
-    try {
-        request.user = jwt.verify(token, process.env.JWT_SECRET);
-        request.headers['x-user-id'] = request.user.userId;
-        request.headers['x-user-role'] = request.user.role;
-
-        const { id1 } = request.body;
-
-        if (Number(id1) !== Number(request.user.userId))
-            return reply.code(403).send({ message: 'No autorizado para aceptar esta friendship' });
-
-    } catch (err) {
-        console.log('[verifySessionFromBody] JWT error:', err.message);
-        return reply.code(401).send({ message: 'No hay una sesión iniciada' });
-    }
-}
-
   
 
 export default { 
@@ -305,8 +230,5 @@ export default {
     updateUserById,
     deleteUserById,
     uploadAvatar,
-    deleteAvatar,
-    verifySession,
-    verifySessionFromPath,
-    verifySessionFromBody
+    deleteAvatar
 }
