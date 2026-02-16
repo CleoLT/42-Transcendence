c6719fd6cbf4b86f8a2a3bfd6b1b209d3bc1f17e
Emilie Sellier <esellier@car8s3.42barcelona.com>
2026-02-16 20:26:01 +0100

    changing opacity

--- srcs/containers/nginx_front/front-end/src/components/iconUtils.jsx
+++ srcs/containers/nginx_front/front-end/src/components/iconUtils.jsx
@@ -3,7 +3,7 @@ import { CorbenBold , CorbenRegular, Sixtyfour } from "./typography"
 
 export function IconsOverlayFrame(){
     return(
-        <div className="h-full w-full bg-shell">
+        <div className="h-full w-full bg-shell opacity-75">
         </div>
     )
 }
