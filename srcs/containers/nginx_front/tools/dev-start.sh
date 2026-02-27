e889a7247dc635bd5a9c709fe9f3212fdc94f3dd
martucs <martalopezcab@gmail.com>
2026-02-27 23:11:16 +0100

    [microserv #12]: fixed dev vite config + updated packages

--- srcs/containers/nginx_front/tools/dev-start.sh
+++ srcs/containers/nginx_front/tools/dev-start.sh
@@ -3,7 +3,14 @@ set -e
 
 CERT_DIR=/certs
 
-echo "[nginx_front] Waiting for api_gateway..."
+echo "[nginx_front] Waiting for certificates..."
+while [ ! -f "$CERT_DIR/nginx_front.crt" ] || \
+      [ ! -f "$CERT_DIR/nginx_front.key" ]; do
+  sleep 1
+done
+
+echo "[nginx_front] Certificates ready."
+
 until nc -z api_gateway 3000; do
   echo "[nginx_front] Waiting for api_gateway..."
   sleep 1
