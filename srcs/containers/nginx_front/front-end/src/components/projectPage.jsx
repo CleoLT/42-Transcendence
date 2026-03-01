4be1fa561811ddf1d1729eb05dc5f87bf04ef1d7
Cléo Le Tron <cle-tron@car14s1.42barcelona.com>
2026-03-01 16:12:28 +0100

    [#86] all information ok in project page

--- srcs/containers/nginx_front/front-end/src/components/projectPage.jsx
+++ srcs/containers/nginx_front/front-end/src/components/projectPage.jsx
@@ -1,24 +1,44 @@
-import { IconsOverlayFrame } from "./iconUtils"
+import { IconsOverlayFrame, ProfilePicture } from "./iconUtils"
 import { Sixtyfour, P, H2, H3, LI, UL } from "./typography"
 
 
-function Card({name, email, role, socialMedia}) {
+function Card({ name, avatar, email, role, socialMedia }) {
     return (
         <div className="
-            flex flex-col border rounded-xl border-greyish
-            px-5 py-3 sm:px-10 justify-between items-center
-            gap-x-2 text-[0.5rem] sm:text-[0.6rem]" >
-            <div className="flex gap-x-2 items-center pb-3">
-                <Sixtyfour>{name}</Sixtyfour>
+            flex flex-row items-center gap-6
+            border rounded-xl border-greyish
+            px-5 py-3 sm:px-10
+            text-[0.5rem] sm:text-[0.6rem]
+        ">
+            <ProfilePicture 
+                src={avatar} 
+                className="w-24 h-24 sm:w-40 sm:h-40"
+            />
 
+            <div className="flex flex-col justify-center">
+
+                <div className="pb-2">
+                    <Sixtyfour>{name}</Sixtyfour>
+                </div>
+
+                <div className="pb-2">
+                    <Sixtyfour>{role}</Sixtyfour>
+                </div>
+
+                <div className="flex flex-col">
+                    <a href={`mailto:${email}`}>
+                        <Sixtyfour>{email}</Sixtyfour>
+                    </a>
+
+                    <a 
+                        href={socialMedia} 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        <Sixtyfour>{socialMedia}</Sixtyfour>
+                    </a>
+                </div>
             </div>
-            <div className="flex gap-x-2 items-center">
-                <Sixtyfour>{role}</Sixtyfour>
-            </div>
-            <button className="pt-3" >
-                <Sixtyfour>{email}</Sixtyfour>
-                <Sixtyfour>{socialMedia}</Sixtyfour>
-            </button>
         </div>
     )
 }
@@ -33,7 +53,14 @@ export function Project() {
             </div>
             <div className="relative w-full max-w-3xl rounded-2xl p-8 max-h-[80vh] overflow-y-auto">
                 <Sixtyfour className="text-2xl">Project</Sixtyfour>
-                <Card name="Cléo" email="cle-tron@student.42barcelona.com" role="backend developer" socialMedia="https://github.com/CleoLT"></Card>
+                <a href="https://github.com/CleoLT/42-Transcendence" target="_blank" rel="noopener noreferrer">
+                    <Sixtyfour>Github Transcendence</Sixtyfour>
+                </a>
+                <Card name="Emilie Sellier" avatar="../../public/avatars/bird.jpg" email="esellier@student.42barcelona.com" role="Product Owner - Frontend Developer" socialMedia="https://github.com/EmilieInData"></Card>
+                <Card name="Marta López Caballer" avatar="../../public/avatars/jellyfish.jpg" email="martalop@student.42barcelona.com" role="Product Manager - DevOps" socialMedia="https://github.com/martucs"></Card>
+                <Card name="Manuel Gimon Caballero" avatar="../../public/avatars/butterfly.jpg" email="mgimon-c@student.42barcelona.com" role="Technical Lead - Backend Developer" socialMedia="https://github.com/mgimon"></Card>
+                <Card name="Cristina Manzanares Urrutia" avatar="../../public/avatars/koi_carp_01.jpg" email="crmanzan@student.42barcelona.com" role="Game Developer" socialMedia="https://github.com/Crima1712"></Card>
+                <Card name="Cléo Le Tron" avatar="../../public/avatars/whale.jpg" email="cle-tron@student.42barcelona.com" role="Backend Developer" socialMedia="https://github.com/CleoLT"></Card>
             </div>
             
 
