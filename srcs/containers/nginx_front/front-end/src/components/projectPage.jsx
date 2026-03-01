76972de10f0454aedcc4f67bc7fbc45e76933b7d
Cléo Le Tron <cle-tron@car14s1.42barcelona.com>
2026-03-01 16:34:24 +0100

    [#86] project page

--- srcs/containers/nginx_front/front-end/src/components/projectPage.jsx
+++ srcs/containers/nginx_front/front-end/src/components/projectPage.jsx
@@ -12,7 +12,7 @@ function Card({ name, avatar, email, role, socialMedia }) {
         ">
             <ProfilePicture 
                 src={avatar} 
-                className="w-24 h-24 sm:w-40 sm:h-40"
+                className="w-24 h-24 sm:w-40 sm:h-40 shrink-0 object-cover"
             />
 
             <div className="flex flex-col justify-center">
@@ -56,11 +56,13 @@ export function Project() {
                 <a href="https://github.com/CleoLT/42-Transcendence" target="_blank" rel="noopener noreferrer">
                     <Sixtyfour>Github Transcendence</Sixtyfour>
                 </a>
-                <Card name="Emilie Sellier" avatar="../../public/avatars/bird.jpg" email="esellier@student.42barcelona.com" role="Product Owner - Frontend Developer" socialMedia="https://github.com/EmilieInData"></Card>
-                <Card name="Marta López Caballer" avatar="../../public/avatars/jellyfish.jpg" email="martalop@student.42barcelona.com" role="Product Manager - DevOps" socialMedia="https://github.com/martucs"></Card>
-                <Card name="Manuel Gimon Caballero" avatar="../../public/avatars/butterfly.jpg" email="mgimon-c@student.42barcelona.com" role="Technical Lead - Backend Developer" socialMedia="https://github.com/mgimon"></Card>
-                <Card name="Cristina Manzanares Urrutia" avatar="../../public/avatars/koi_carp_01.jpg" email="crmanzan@student.42barcelona.com" role="Game Developer" socialMedia="https://github.com/Crima1712"></Card>
-                <Card name="Cléo Le Tron" avatar="../../public/avatars/whale.jpg" email="cle-tron@student.42barcelona.com" role="Backend Developer" socialMedia="https://github.com/CleoLT"></Card>
+                <div className="flex flex-col gap-6">
+                    <Card name="Emilie Sellier" avatar="/avatars/bird.jpg" email="esellier@student.42barcelona.com" role="Product Owner - Frontend Developer" socialMedia="https://github.com/EmilieInData"></Card>
+                    <Card name="Marta López Caballer" avatar="/avatars/jellyfish.jpg" email="martalop@student.42barcelona.com" role="Product Manager - DevOps" socialMedia="https://github.com/martucs"></Card>
+                    <Card name="Manuel Gimon Caballero" avatar="/avatars/butterfly.jpg" email="mgimon-c@student.42barcelona.com" role="Technical Lead - Backend Developer" socialMedia="https://github.com/mgimon"></Card>
+                    <Card name="Cristina Manzanares Urrutia" avatar="/avatars/koi_carp_01.jpg" email="crmanzan@student.42barcelona.com" role="Game Developer" socialMedia="https://github.com/Crima1712"></Card>
+                    <Card name="Cléo Le Tron" avatar="/avatars/whale.jpg" email="cle-tron@student.42barcelona.com" role="Backend Developer" socialMedia="https://github.com/CleoLT"></Card>
+                </div>
             </div>
             
 
